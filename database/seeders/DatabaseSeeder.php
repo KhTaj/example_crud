399b9b99c954a2ba7f490be88fc48a887e5c2998
Khandoker Tajuddin <khtajuddin97@gmail.com>
2022-10-04 01:54:38 +0600

    php artisan db:seed ITS RUN SUCCESSFULLY

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        echo "Tajuddin is calling database seeder run method.";
         // \App\Models\User::factory(10)->create();
     }
 }
