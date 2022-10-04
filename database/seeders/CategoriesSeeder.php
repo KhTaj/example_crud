bc1ea0ccdce3a6b59699ee39486d6eaa609a137a
Khandoker Tajuddin <khtajuddin97@gmail.com>
2022-10-04 11:05:20 +0600

    working in categoriesSeeder

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,10 +17,13 @@ class CategoriesSeeder extends Seeder
     public function run()
     {
          // echo "Tajuddin is calling database seeder run method.";
-        // DB::table('categories')->insert([
-        //     'name'=>'Men',
-        //     'is_active' => true
-        // ]);
+        DB::table('categories')->insert([
+            'name'=>'Men',
+            'is_active' => true,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+
+        ]);
         Category::create([
             'name'=> 'Woman',
             'is_active'=>true
